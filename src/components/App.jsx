1d53c5bbf396e7e67c78135039d5cde4d74a344e
Julia-Kaim <julia.kaim08@gmail.com>
2023-07-06 19:49:46 +0200

    feature: createAsyncThunk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { useSelector } from 'react-redux';
+
+import { ContactList } from '../components/ContactList/ContactList';
+import { Filter } from '../components/Filter/Filter';
+import { ContactForm } from '../components/ContactForm/ContactForm';
+import styles from './App.module.css';
+import { selectContacts } from 'redux/selectors';
+import { selectFilter } from 'redux/selectors';
+
 export const App = () => {
+  const filtered = useSelector(selectFilter);
+  const contacts = useSelector(selectContacts);
+
+  const filterContact = e => {
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filtered.toLowerCase())
+    );
+    return filteredContacts;
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.Layout}>
+      <h1 className={styles.titleContacts}>Phonebook</h1>
+      <ContactForm />
+      <div className={styles.allContacts}>All contacts: {contacts.length}</div>
+      <h2 className={styles.titleContacts}>Contacts</h2>
+      <Filter />
+      <ContactList listContact={filterContact()} />
     </div>
   );
 };
